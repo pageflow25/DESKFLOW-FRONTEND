a36ff6f8157ff73cbe421d96583033fade125499
arthuzao <arthur88727771@gmail.com>
2026-01-12 12:32:56 -0300

    fix: switch from BrowserRouter to HashRouter for routing

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { AuthProvider } from './contexts/AuthContext'
 import ProtectedRoute from './components/ProtectedRoute'
